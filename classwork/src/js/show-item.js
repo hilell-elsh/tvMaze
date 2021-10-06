d479340a24b99c7bf2e4e93aed314250ebcdd0cb
hilell-elsh <84779482+hilell-elsh@users.noreply.github.com>
2021-10-06 12:23:25 +0300

    Delete classwork directory

--- classwork/src/js/show-item.js
+++
@@ -1,8 +0,0 @@
-export function getShowItemComponent(item) {
-	const element = document.createElement('div');
-	element.classList.add('show-item');
-	element.innerHTML = item.name;
-	return {
-		element,
-	};
-}
